53d37d80aebf9fef85a06697a525cd0bc0c4b7d1
albwn123 <albinjoy4743@gmail.com>
2026-06-10 13:29:52 +0530

    Basic Agent Done

---
+++ agent/agent.py
@@ -0,0 +1,52 @@
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+import os
+from dotenv import load_dotenv
+
+# print(os.path.exists('.env'))
+load_dotenv()
+print(os.path.exists('.env'))
+# print()
+llm = ChatGroq(
+    model='llama-3.3-70b-versatile',
+    temperature = 0.2,
+    api_key=os.getenv('GROQ_API_KEY')
+)
+
+prompt = ChatPromptTemplate.from_template(
+'''
+You are a banking assistant.
+Rules:
+1. Answer Only banking related questions and you may answer questions about your role and capabilities.
+2.Banking TOpics include:
+    -Savings Accounts
+    -Current accounts
+    -loans
+    -credit cards
+    -FIxed Deposits
+    -Debit Cards
+    -Interest Rated
+    -Banking Regualtions
+    -Digital Banking
+3.If the question is not related to banking, then respond with:
+'Sorry, i am a banking assitant. I only answer banking related questions.'
+4.If the question is like "WHo are you" then respond with:
+'Am a banking assitant. I will help you for the banking related queries.'
+Question:{question}
+'''
+)
+
+chain = prompt | llm
+
+while True:
+    query = input('Ask a Question:')
+
+    if query.lower() == 'exit':
+        break
+
+    response = chain.invoke({
+        'question':query
+    })
+
+    print('\nAnswer : ')
+    print(response.content)
