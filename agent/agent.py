40585ff6136e83fa7ca8a66c8675fe131424994e
albwn123 <albinjoy4743@gmail.com>
2026-06-10 16:00:09 +0530

    Created the project structure

--- agent/agent.py
+++ agent/agent.py
@@ -29,10 +29,12 @@ Rules:
     -Interest Rated
     -Banking Regualtions
     -Digital Banking
+
 3.If the question is not related to banking, then respond with:
 'Sorry, i am a banking assitant. I only answer banking related questions.'
 4.If the question is like "WHo are you" then respond with:
 'Am a banking assitant. I will help you for the banking related queries.'
+5. If the question is greeting questions then just Send a greeting reply and add "Am a banking assistant how can i help you?"
 Question:{question}
 history:{chat_history}
 '''
@@ -42,15 +44,12 @@ chain = prompt | llm
 
 while True:
     query = input('Ask a Question:')
-
     if query.lower() == 'exit':
         break
-
     response = chain.invoke({
         'question':query,
         'chat_history':history
     })
-
     print('\nAnswer : ')
     print(response.content)
     history.append(response.content)
